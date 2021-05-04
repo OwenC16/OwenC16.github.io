0088796e7cf46a9b1b14d9dbd1e7036f4d1db667
OwenC16 <83368252+OwenC16@users.noreply.github.com>
2021-05-04 18:26:44 -0400

    Update caisse.js

--- caisse.js
+++ caisse.js
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <script>
+var prix = prompt ("Quelle est le prix de l'article");
+var quantite = prompt ("Quelle est la quantité de l'article");
+var avant = parseFloat (prix * quantite);
+var apres = parseFloat (avant * 1.13);
+apres = Math.floor (apres * 100) / 100;
+(apres.toFixed(2));
+alert ("Ton prix totale est égale à " + apres + " dollars.")
+</script>
+  </body>
+</html>
 
